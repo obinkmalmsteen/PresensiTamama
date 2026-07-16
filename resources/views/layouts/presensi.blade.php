18832d40b58e76db6ca7e7df56a77d99d9d3c143
obinkmalmsteen <obink.w.malmsteen@gmail.com>
2026-07-17 00:36:38 +0700

    viewport

--- resources/views/layouts/presensi.blade.php
+++ resources/views/layouts/presensi.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
     <meta http-equiv="Pragma" content="no-cache" />
     <meta http-equiv="Expires" content="0" />
-    <meta name="viewpophp"
+    <meta name="viewport"
         content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover" />
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
